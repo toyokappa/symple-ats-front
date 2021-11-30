5287099cb74d811fc922fcbab0bc94c3bb4c5866
Yuta Toyokawa <kppg42@gmail.com>
2021-11-30 22:39:40 +0900

    グラフのラベルをデザイン

--- pages/analytics/index.vue
+++ pages/analytics/index.vue
@@ -1,18 +1,32 @@
 <template lang="pug">
 .w-full
-  .p-8.h-screen
-    line-chart(:chartData="chartData" :options="options" :height="200")
+  .p-8
+    .relative
+      .absolute.flex.w-full
+        .flex-1.p-3(v-for="(value, index) in displayValues" :key="index")
+          .text-gray-500 {{ chartLabels[index] }}
+          .text-2xl.font-bold {{ value }}
+          .font-bold.text-gray-500(v-if="index < displayValues.length - 1")
+            span.text-base.mr-px {{ valuePercentage[index] }}
+            span.text-xs %
+      line-chart(:chartData="chartData" :options="options" :height="200")
 </template>
 
 <script>
 export default {
   data() {
     return {
-      dataValues: [10, 5, 4, 4, 3, 3],
+      dataValues: [60, 36, 15, 10, 6, 6],
       chartLabels: ['書類選考', '1次面接', '2次面接', '内定', '内定承諾', '']
     }
   },
   computed: {
+    displayValues() {
+      return this.dataValues.filter((_, index) => index < this.dataValues.length - 1)
+    },
+    valuePercentage() {
+      return this.dataValues.map((value, index) => Math.floor(this.dataValues[index + 1] / value * 10 * 100) / 10)
+    },
     chartData() {
       return {
         datasets: [
@@ -29,16 +43,23 @@ export default {
         legend: {
           display: false,
         },
+        labels: {
+          display: false
+        },
         scales: {
           xAxes: [{
+            ticks: {
+              display: false
+            }
           }],
           yAxes: [{
             ticks: {
               display: false,
-              beginAtZero: true
+              beginAtZero: true,
             },
             gridLines: {
-              display: false
+              display: false,
+              drawTicks: false
             }
           }],
         },
@@ -56,5 +77,5 @@ export default {
 }
 </script>
 
-<style lang="sass">
+<style lang="sass" scoped>
 </style>
--- plugins/chart.js
+++ plugins/chart.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import { Line, mixins } from 'vue-chartjs';
+
 const { reactiveProp } = mixins;
 
 Vue.component('line-chart', {
