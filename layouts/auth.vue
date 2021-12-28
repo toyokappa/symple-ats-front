9e5547eedaff4b739b2e37dbfabd8902ac2bab84
Yuta Toyokawa <kppg42@gmail.com>
2021-12-29 01:13:55 +0900

    ログインUIをVuetifyで実装

---
+++ layouts/auth.vue
@@ -0,0 +1,8 @@
+<template lang="pug">
+v-app
+  v-content
+    v-container
+      nuxt
+</template>
+
+<style lang="sass"></style>
--- pages/signin/index.vue
+++ pages/signin/index.vue
@@ -1,50 +1,48 @@
 <template lang="pug">
-.min-h-full.flex.items-center.justify-center.py-12.px-4
-  .max-w-md.w-full.space-y-8
-    div
-      h2.text-center.text-3xl.font-extrabold ログイン
-      p.mt-2.text-center.text-sm.text-gray-600 もしくは 
-        a.font-medium.text-indigo-600(href="#") 利用を申請する
-    form.mt-8.space-y-6(@submit.prevent="signin")
-      input(type="hidden" name="remember" value="true")
-      .rounded.shadow-sm.-space-y-px
-        div
-          label.sr-only(for="email")
-          input#email.apperrance-none.rounded-none.relative.block.w-full.px-3.py-2.border.border-gray-300.placeholder-gray-300.rounded-t(
-            v-model="recruiter.email"
-            type="email"
-            name="email"
-            autocomplete="email"
-            required
-            placeholder="メールアドレス"
-          )
-        div
-          label.sr-only(for="password")
-          input#password.apperance-none.rounded-none.relative.block.w-full.px-3.py-2.border.border-gray-300.placeholder-gray-300.rounded-b(
-            v-model="recruiter.password"
-            type="password"
-            name="password"
-            autocomplete="password"
-            required
-            placeholder="パスワード"
-          )
-      .flex.items-center.justify-between
-        .flex.items-center
-          input#remember-me.h-4.w-4.text-indigo-600.border-gray-300.rounded(
-            type="checkbox"
-            name="remember-me"
-          )
-          label.ml-2.block.text-sm(for="remember-me") ログイン状態を保存する
-        .text-sm
-          a.font-medium.text-indigo-600(href="#") パスワードを忘れた方
-      div
-        button.group.lelative.w-full.flex.justify-center.py-2.px-4.border.border-transparent.text-sm.font-medium.rounded.text-white.bg-indigo-600(
-          type="submit"
-        ) ログイン
+v-card.py-16.mx-auto(
+  flat
+  max-width="480"
+)
+  .mb-4
+    v-icon.d-block.text-center.mb-3(
+      color="black"
+      size="56"
+    ) mdi-lock
+    h1.text-center ログイン
+    p.text-center.subtitle-2.mb-0 もしくは 
+      a 利用を申請する
+  v-form(@submit.prevent="signin")
+    .mb-4
+      v-text-field.body-2.mb-3(
+        v-model="recruiter.email"
+        type="email"
+        autofocus
+        dense
+        outlined
+        hide-details="auto"
+        placeholder="メールアドレス"
+      )
+      v-text-field.body-2.mb-3(
+        v-model="recruiter.password"
+        type="password"
+        dense
+        outlined
+        hide-details="auto"
+        placeholder="パスワード"
+      )
+    .mb-4
+      a.d-block.text-right.subtitle-2 パスワードを忘れた場合
+    .login-btn
+      v-btn(
+        type="submit"
+        depressed
+        block
+      ) ログイン
 </template>
 
 <script>
 export default {
+  layout: 'auth',
   data() {
     return {
       recruiter: {
