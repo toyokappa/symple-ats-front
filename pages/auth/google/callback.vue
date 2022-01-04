1d011ece654c5cf33998bc4793d22cdb56cb7b49
Yuta Toyokawa <kppg42@gmail.com>
2022-01-04 20:05:48 +0900

    連携リダイレクト後の処理

---
+++ pages/auth/google/callback.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+</template>
+
+<script>
+export default {
+  async fetch({ query, $axios, redirect }) {
+    await $axios.post('/auth/google/callback', {
+      code: query.code,
+    })
+    redirect('/')
+  },
+}
+</script>
